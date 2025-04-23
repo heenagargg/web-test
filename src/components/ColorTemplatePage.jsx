22c78bca882e550dbb7d03891f3e3daa4569a520
nishantpoplify <nishant.thakur@poplify.com>
2025-04-23 15:23:19 +0530

    Feature: Logo Color Picker

--- src/components/ColorTemplatePage.jsx
+++ src/components/ColorTemplatePage.jsx
@@ -1,28 +1,82 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./ColorTemplatePage.css";
 import emptyImage from "../assets/icons8-empty-64.png";
 import { useLocation, useNavigate } from "react-router";
 import { IoIosArrowRoundBack } from "react-icons/io";
 import { IoIosArrowRoundForward } from "react-icons/io";
+import ColorThief from "colorthief";
+const colorThief = new ColorThief();
 const ColorTemplatePage = () => {
+  const imgRef = useRef();
   const [theme, setTheme] = useState("material");
-  const [colors, setColors] = useState("blue-grey");
+  const [logoURL, setLogoURL] = useState("");
+  const [colors, setColors] = useState([
+    [[26, 188, 156], [108, 117, 125]],
+    [[40, 53, 102], [152, 100, 36]],
+    [[58, 91, 204], [137, 137, 137]],
+    [[227, 54, 101], [137, 137, 137]],
+    [[90, 94, 50], [219, 199, 91]],
+    [[95, 25, 40], [222, 135, 194]],
+    [[88, 51, 241], [137, 137, 137]],
+    [[130, 31, 160], [244, 199, 48]],
+    [[141, 109, 54], [158, 189, 89]],
+    [[237, 103, 68], [167, 162, 160]]
+  ]);
+  const [activeColor, setActiveColor] = useState([[26, 188, 156], [108, 117, 125]]);
   const [fonts, setFonts] = useState("material-font");
-  const navigate=useNavigate()
-    const location = useLocation();
-    const returnData = location.state.jsonData ?? {};
+  const navigate = useNavigate()
+  const location = useLocation();
+  const returnData = location.state.jsonData ?? {};
   console.log("jsonData", returnData);
-  console.log("colors",colors)
-  console.log("theme",theme)
-  console.log("fonts",fonts)
+  console.log("color", activeColor)
+  console.log("theme", theme)
+  console.log("fonts", fonts)
+  const allowedFileTypes = ['image/png', 'image/jpeg', 'image/jpg', 'image/bmp']
+
+  const handleLogoUpload = (e) => {
+    e.preventDefault();
+    let elem = e.currentTarget;
+
+    let selectedFiles = elem.files;
+    if (selectedFiles.length) {
+      let fileObj = selectedFiles[0];
+
+      if (allowedFileTypes.includes(fileObj.type)) {
+        var imgDataUrl = URL.createObjectURL(fileObj);
+        imgRef.current.src = imgDataUrl;
+      } else {
+        elem.value = "";
+        alert("Please select an image file");
+      }
+    }
+  }
+
+  const getColors = () => {
+    // Make sure image is loaded
+    if (imgRef.current.complete) {
+      var colorPallete = colorThief.getPalette(imgRef.current, 2)
+      colors.push(colorPallete)
+      setColors([...colors])
+      setActiveColor(colorPallete)
+    }
+  }
+
+  const generateColor = (color, index) => {
+    if (typeof (color) === "object" && color.length >= index) {
+      return "rgb(" + color[index].toString() + ")";
+    } else {
+      return "rgb(0,0,0)";
+    }
+  }
   return (
     <>
-      <div className="website-customizer-box">
+      <div className="website-customizer-box" style={{ '--accentColor': generateColor(activeColor, 0), '--secondaryColor': generateColor(activeColor, 1) }} >
         <section
-          className={`website-customizer-container ${fonts} ${theme} ${colors}`}
+          className={`website-customizer-container ${fonts} ${theme}`}
         >
           <div className="website-customizer-body-container">
             <div className="website-customizer-body">
+              <img src="" alt="Site Logo" ref={imgRef} className="site-logo-main" onLoad={getColors} />
               <div className="website-customizer-image-section mb25">
                 <div className="section-image-container">
                   <img src={emptyImage} alt="empty-img" />
@@ -71,12 +125,12 @@ const ColorTemplatePage = () => {
                   <div className="section-actions-container">
                     <div>
                       <i className="db-icon db-icon-back">
-                      <IoIosArrowRoundBack />
+                        <IoIosArrowRoundBack />
                       </i>
                     </div>
                     <div>
                       <i className="db-icon db-icon-back2">
-                      <IoIosArrowRoundForward />
+                        <IoIosArrowRoundForward />
                       </i>
                     </div>
                   </div>
@@ -130,179 +184,29 @@ const ColorTemplatePage = () => {
           <div className="color-picker-container mb24">
             <span className="title-small mb6">Colors</span>
             <div className="db-list">
-              <div
-                onClick={() => setColors("cyan-grey")}
-                className={`AI color-item suggested ng-star-inserted cyan-grey ${
-                  colors === "cyan-grey" ? "selected" : ""
-                }  `}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(26, 188, 156)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(108, 117, 125)" }}
-                  ></div>
+              {colors.map((color) => {
+                console.log(color);
+                return <div
+                  onClick={() => setActiveColor(color)}
+                  className={`AI color-item suggested ${color === activeColor ? "selected" : ""
+                    }`}
+                >
+                  <div className="color-set-container">
+                    <div
+                      className="color"
+                      style={{ backgroundColor: generateColor(color, 0) }}
+                    ></div>
+                    <div
+                      className="color"
+                      style={{ backgroundColor: generateColor(color, 1) }}
+                    ></div>
+                  </div>
                 </div>
-              </div>
-              <div
-                onClick={() => setColors("blue-brown")}
-                className={`Indigo color-item ng-star-inserted ${
-                  colors === "blue-brown" ? "selected" : ""
-                } `}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(40, 53, 102)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(152, 100, 36)" }}
-                  ></div>
-                </div>
-              </div>
-              <div
-                onClick={() => setColors("blue-grey")}
-                className={`Han blue color-item ng-star-inserted  ${
-                  colors === "blue-grey" ? "selected" : ""
-                } `}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(58, 91, 204)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(137, 137, 137)" }}
-                  ></div>
-                </div>
-              </div>
-              <div
-                onClick={() => setColors("pink-grey")}
-                className={`Orchid color-item pink ng-star-inserted  ${
-                  colors === "pink-grey" ? "selected" : ""
-                }`}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(227, 54, 101)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(137, 137, 137)" }}
-                  ></div>
-                </div>
-              </div>
-              <div
-                onClick={() => setColors("green-parrot")}
-                className={`Basque color-item green ng-star-inserted ${
-                  colors === "green-parrot" ? "selected" : ""
-                }`}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(90, 94, 50)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(219, 199, 91)" }}
-                  ></div>
-                </div>
-              </div>
-              <div
-                onClick={() => setColors("red-pink")}
-                className={`Red berry color-item ng-star-inserted ${
-                  colors === "red-pink" ? "selected" : ""
-                }`}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(95, 25, 40)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(222, 135, 194)" }}
-                  ></div>
-                </div>
-              </div>
-              <div
-                onClick={() => setColors("darkblue-grey")}
-                className={`Purplish blue color-item ng-star-inserted ${
-                  colors === "darkblue-grey" ? "selected" : ""
-                }`}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(88, 51, 241)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(137, 137, 137)" }}
-                  ></div>
-                </div>
-              </div>
-              <div
-                onClick={() => setColors("purple-yellow")}
-                className={`Obsidian color-item shell ng-star-inserted ${
-                  colors === "purple-yellow" ? "selected" : ""
-                }`}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(130, 31, 160)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(244, 199, 48)" }}
-                  ></div>
-                </div>
-              </div>
-              <div
-                onClick={() => setColors("brown-green")}
-                className={`Beagle brown color-item ng-star-inserted ${
-                  colors === "brown-green" ? "selected" : ""
-                }`}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(141, 109, 54)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(158, 189, 89)" }}
-                  ></div>
-                </div>
-              </div>
-              <div
-                onClick={() => setColors("orange-grey")}
-                className={`Aurora color-item orange ng-star-inserted ${
-                  colors === " orange-grey" ? "selected" : ""
-                }`}
-              >
-                <div className="color-set-container">
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(237, 103, 68)" }}
-                  ></div>
-                  <div
-                    className="color"
-                    style={{ backgroundColor: "rgb(167, 162, 160)" }}
-                  ></div>
-                </div>
-              </div>
+              })}
+
             </div>
           </div>
-          <div className="style-picker-container">
+          <div className="style-picker-container mb24">
             <span className="title-small mb10">Themes</span>
             <div className="db-styles mt10">
               <div
@@ -310,9 +214,8 @@ const ColorTemplatePage = () => {
                   setTheme("classic");
                   setFonts("classic-font");
                 }}
-                className={`classNameic db-style ng-star-inserted ${
-                  theme === "classic" ? "selected" : ""
-                }`}
+                className={`classNameic db-style ${theme === "classic" ? "selected" : ""
+                  }`}
               >
                 <div>
                   <span className="theme-title">Classic</span>
@@ -329,9 +232,8 @@ const ColorTemplatePage = () => {
                   setTheme("flat");
                   setFonts("flat-font");
                 }}
-                className={`db-style flat ng-star-inserted ${
-                  theme === "flat" ? "selected" : ""
-                }`}
+                className={`db-style flat ${theme === "flat" ? "selected" : ""
+                  }`}
               >
                 <div>
                   <span className="theme-title">Flat</span>
@@ -348,9 +250,8 @@ const ColorTemplatePage = () => {
                   setTheme("material");
                   setFonts("material-font");
                 }}
-                className={`db-style material ng-star-inserted ${
-                  theme === "material" ? "selected" : ""
-                }`}
+                className={`db-style material ${theme === "material" ? "selected" : ""
+                  }`}
               >
                 <div>
                   <span className="theme-title">Material</span>
@@ -367,9 +268,8 @@ const ColorTemplatePage = () => {
                   setTheme("minimalistic");
                   setFonts("minimalistic-font");
                 }}
-                className={`db-style minimalistic ng-star-inserted ${
-                  theme === "minimalistic" ? "selected" : ""
-                }`}
+                className={`db-style minimalistic ${theme === "minimalistic" ? "selected" : ""
+                  }`}
               >
                 <div>
                   <span className="theme-title">Minimalistic</span>
@@ -386,9 +286,8 @@ const ColorTemplatePage = () => {
                   setTheme("soft");
                   setFonts("soft-font");
                 }}
-                className={`db-style soft ng-star-inserted ${
-                  theme === "soft" ? "selected" : ""
-                }`}
+                className={`db-style soft ${theme === "soft" ? "selected" : ""
+                  }`}
               >
                 <div>
                   <span className="theme-title">Soft</span>
@@ -402,37 +301,44 @@ const ColorTemplatePage = () => {
               </div>
             </div>
           </div>
+          <div className="custom-color-container mb24">
+            <span class="title-small mb6">Add Logo</span>
+            <div className="logo-color-theif">
+              <input type="file" accept=".png,.jpg,.jpeg" onChange={handleLogoUpload} />
+            </div>
+          </div>
+
         </div>
       </div>
-      <div className="db-actions-line ng-star-inserted active">
-      <div className="footcol product-by">
-            Powered by{" "}
-            <a href="https://42works.net/" target="_blank">
-              {/* <img src="./42-logo.svg" /> Works */}
-              <img src="../../svg-image-1.svg" alt="42 Works" />
-            </a>
-            .{" "}All Rights Reserved.
-          </div>
+      <div className="db-actions-line active">
+        <div className="footcol product-by">
+          Powered by{" "}
+          <a href="https://42works.net/" target="_blank">
+            {/* <img src="./42-logo.svg" /> Works */}
+            <img src="../../svg-image-1.svg" alt="42 Works" />
+          </a>
+          .{" "}All Rights Reserved.
+        </div>
         <ul className="db-steps item 4">
-          <li className="active ng-star-inserted">
+          <li className="active">
             <span>SITE INFO</span>
           </li>
-          <li className="active ng-star-inserted">
+          <li className="active">
             <span>PAGES</span>
           </li>
-          <li className="ng-star-inserted active">
+          <li className="active">
             <span>COLORS &amp; FONTS</span>
           </li>
         </ul>
-       <div className="back-generate-div">
-       <a className="ds-btn-m ds-btn-outline item" onClick={()=>navigate('/site-structure', {state:{returnData}})} >
-          <span >Back</span>
-        </a>
-        <a className="ds-btn-purple-blue ds-btn-m item ng-star-inserted">
-          <i className="db-icon db-icon-ds-ai-generate mr8"></i>
-          <span>Generate</span>
-        </a>
-       </div>
+        <div className="back-generate-div">
+          <a className="ds-btn-m ds-btn-outline item" onClick={() => navigate('/site-structure', { state: { returnData } })} >
+            <span >Back</span>
+          </a>
+          <a className="ds-btn-purple-blue ds-btn-m item">
+            <i className="db-icon db-icon-ds-ai-generate mr8"></i>
+            <span>Generate</span>
+          </a>
+        </div>
       </div>
     </>
   );
